c6bb00a379daa5efc203f6ed05f63afef2571aa5
Roberto <ro89585@gmail.com>
2022-11-15 17:46:55 -0800

    Show route connected

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -12,14 +12,25 @@ function home () {
   }
   
   function index (data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, index) => {
       return (
-        <div>
-          <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
+        <div className="col-sm-6">
+          <h2>
+            <a href={`/places/${index}`} >
+              {place.name}
+            </a>
+          </h2>
+          <p className="text-center">
+            {place.cuisines}
+          </p>
+          <img src={place.pic} alt={place.name} />
+          <p className="text-center">
+            Located in {place.city}, {place.state}
+          </p>
         </div>
       )
     })
+    
     return (
       <Def>
           <main>
