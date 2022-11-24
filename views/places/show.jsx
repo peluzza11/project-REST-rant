2a8e8c2cb4cefe12c4512250a0a7d31275f3d1e4
Roberto <ro89585@gmail.com>
2022-11-24 15:57:53 -0800

    Adding comments to REST-Rant

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,26 +1,36 @@
-const React = require('react')
-const Def = require('../default')
-
 function show (data) {
-    return (
-        <Def>
-          <main>
-            <h1>{ data.place.name }</h1>
-            <a href="" className="btn btn-warning"> 
-  Edit
-</a>  
-<form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-  <button type="submit" className="btn btn-danger">
-    Delete
-  </button>
-</form> 
-
-    
-
-
-          </main>
-        </Def>
-    )
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
+  return (
+      <Def>
+        <main>
+          <div className="row">
+            ...
+          </div>
+          <hr />
+          <h2>Comments</h2>
+          {comments}
+        </main>
+      </Def>
+  )
 }
 
 module.exports = show
+
